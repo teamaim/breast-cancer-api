9a2f8e1b112ba4d61a31b3e523798a8ab4565245
teamaim <154457489+teamaim@users.noreply.github.com>
2025-06-07 15:03:01 +0300

    Add files via upload

--- breast-cancer-api/app.py
+++ breast-cancer-api/app.py
@@ -113,4 +113,6 @@ def predict_detailed():
     return jsonify(report)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
