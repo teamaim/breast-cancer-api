f574eceae26bdf39b1fab7623973b518f0df8fcb
teamaim <154457489+teamaim@users.noreply.github.com>
2025-06-08 16:27:49 +0300

    Update app.py

--- breast-cancer-api/app.py
+++ breast-cancer-api/app.py
@@ -114,5 +114,5 @@ def predict_detailed():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True, use_reloader=False)
 
